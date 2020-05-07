cad64069016ba3ff9f8cdab4fac9aedace58c90c
Zohar Babin <z.babin@gmail.com>
2020-05-07 12:25:24 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Kaltura Meeting Embed Sample
+
+This app shows how to embed Kaltura Meetings rooms (it skips the room creation part).
+This is a very basic node express based app (generated using [Express Generator](https://expressjs.com/en/starter/generator.html)).
+
+# To run
+1. Checkout/Download the files
+1. `npm install`
+1. Copy `config.template.json` to `config.json`
+1. Open `config.json`, configure according to the instructions and remove all comments
+1. Run - 
+   * On Mac/Linux - `DEBUG=kalturameeting:* npm start`
+   * Windows - `set DEBUG=myapp:* & npm start`
+1. Load http://localhost:3000/ in your browser to access the app.
+
+# Where to get help
+* Join the [Kaltura Community Forums](https://forum.kaltura.org/) to ask questions or start discussions
+* Read the [Code of conduct](https://forum.kaltura.org/faq) and be patient and respectful
+
+# Get in touch
+You can learn more about Kaltura and start a free trial at: http://corp.kaltura.com    
+Contact us via Twitter [@Kaltura](https://twitter.com/Kaltura) or email: community@kaltura.com  
+We'd love to hear from you!
+
+# License and Copyright Information
+All code in this project is released under the [AGPLv3 license](http://www.gnu.org/licenses/agpl-3.0.html) unless a different license for a particular library is specified in the applicable library path.   
+
+Copyright © Kaltura Inc. All rights reserved.
+
+### Open Source Libraries
+Review the [list of Open Source 3rd party libraries](open-source-libraries.md) used in this project.
---
+++ routes/launchroom.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var kaltura = require('kaltura-client');
+var router = express.Router();
+
+/* GET redirect back to register page if user gets here without registering */
+router.get('/', function(req, res, next) {
+  res.redirect('/');
+});
+
+/* POST launch a room with params. */
+router.post('/', function(req, res, next) {
+  const config = new kaltura.Configuration();
+  config.serviceUrl = req.app.get('kserviceUrl');
+  const client = new kaltura.Client(config);
+  const apiSecret = req.app.get('kapiSecret');
+  const partnerId = req.app.get('kpartnerId'); 
+  const expiry = req.app.get('ksessionDuration');
+  const type = kaltura.enums.SessionType.USER;
+
+  // Read about rooms management and all the room launch customizatin options here:
+  // https://pitch.kaltura-pitch.com/message/b68f06feaf6245816ec0c14f770ba97589c8f0c2a70d4cd038f2b2b94ed4
+
+  // Map room number to resourceId. Resources (virtual rooms) are created via the scheduleResource.add API:
+  // https://developer.kaltura.com/console/service/scheduleResource/action/add
+  resourceIds = req.app.get('kresourceIds');
+  let resourceId = null;
+  let room = parseInt(req.body.room);
+  if (resourceIds.hasOwnProperty(room)) {
+    resourceId = resourceIds[room];
+  } else {
+    res.status(400).send("Error: invalid room number: "+room + " ("+req.body.room+")");
+  }
+
+  // Set priveleges parameter for Kaltura Session (KS) generation.
+  // For userContextual role:
+  //   0 = instructor
+  //   3 = guest
+  // Mandatory fields: role, userContextualRole, resourceId (or eventId)
+  let userContextualRole = req.body.role == "instructor" ? "0" : "3";  
+  let privileges = "role:viewerRole,userContextualRole:" + userContextualRole + ",resourceId:" + resourceId + 
+    ",firstName:" + req.body.firstName + ",lastName:" + req.body.lastName;
+  let userId = req.body.email; // using email as userId, but it can be any alphanumeric unique string if you prefer
+
+  // Get a Kaltura Session
+  kaltura.services.session.start(apiSecret, userId, type, partnerId, expiry, privileges)
+  .execute(client)
+  .then(result => {
+      let roomUrl = "https://" + partnerId + ".kaf.kaltura.com/virtualEvent/launch?ks=" + result;
+      // Render the room -
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.render('launchroom', { roomUrl: roomUrl});
+  });
+});
+
+module.exports = router;
